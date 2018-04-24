61c86dc8f66c07fece502c5004524735a9f2fba0
Tony <xuafu86@163.com>
2018-04-24 23:09:23 +0800

    change

--- src/components/login/Login1.js
+++ src/components/login/Login1.js
@@ -3,29 +3,29 @@ import '../../css/style.css';
 
 class Login1 extends Component {
 
-    render() {
-        return (
-            <div className="login-05">
-                <div className="fifth-login">
-                    <h2>LOGIN FORM</h2>
-                    <form className="five">
-                        <li>
-                            <input type="text" className="text" value="EMAIL" />
-                            <label className="icon5 user2"/>
-                        </li>
-                        <li className="blue">
-                            <input type="password" value="PASSWORD" />
-                            <label className =  "icon5 lock2"/>
-                        </li>
-                        <div className="submit-five">
-                            <input type="submit" onClick="myFunction()" value="LOG IN" />
-                        </div>
-                    </form>
-                </div>
+  render() {
+    return (
+      <div className="login-05">
+        <div className="fifth-login">
+          <h2>LOGIN FORM</h2>
+          <form className="five">
+            <li>
+              <input type="text" className="text" value="EMAIL" />
+              <label className="icon5 user2"/>
+            </li>
+            <li className="blue">
+              <input type="password" value="PASSWORD" />
+              <label className =  "icon5 lock2"/>
+            </li>
+            <div className="submit-five">
+              <input type="submit" onClick="myFunction()" value="LOG IN" />
             </div>
+          </form>
+        </div>
+      </div>
 
     );
-    }
+  }
 }
 
 export default Login1;
