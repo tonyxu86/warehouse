fa0b121c583a77d00407a111ad96534b8e6a06b0
Tony <xuafu86@163.com>
2018-04-24 23:11:24 +0800

    change

--- src/components/login/Login1.js
+++ src/components/login/Login1.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../../css/style.css';
 
 class Login1 extends Component {
@@ -10,15 +10,15 @@ class Login1 extends Component {
           <h2>LOGIN FORM</h2>
           <form className="five">
             <li>
-              <input type="text" className="text" value="EMAIL" />
+              <input type="text" className="text" value="EMAIL"/>
               <label className="icon5 user2"/>
             </li>
             <li className="blue">
-              <input type="password" value="PASSWORD" />
-              <label className =  "icon5 lock2"/>
+              <input type="password" value="PASSWORD"/>
+              <label className="icon5 lock2"/>
             </li>
             <div className="submit-five">
-              <input type="submit" onClick="myFunction()" value="LOG IN" />
+              <input type="submit" onClick="myFunction()" value="LOG IN"/>
             </div>
           </form>
         </div>
